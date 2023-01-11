ee541a4231753ba048c792e390c294fb1940c9b8
Lindsey <69260837+lindzfaith@users.noreply.github.com>
2023-01-11 10:35:11 -0500

    Deleted accidental directory.

--- e-commerce/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Cart } from './Cart';
-export { default as FooterBottom } from './FooterBottom';
-export { default as HeroBanner } from './HeroBanner';
-export { default as Footer } from './Footer';
-export { default as Layout } from './Layout';
-export { default as NavigationBar } from './NavigationBar';
-export { default as Product } from './Product';
